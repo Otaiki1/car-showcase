ae7a3a8fa95a534b9fa8fc36f100ab7fb126055b
Otaiki1 <otaikisadiq2804@gmail.com>
2023-09-10 10:55:40 +0100

    feat: add catalogue section

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Footer, NavBar };
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
